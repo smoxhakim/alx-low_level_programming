e5b3cd8eb8b477c85fa9c3d7f0479bfa3e356415
smoxhakim <joulal1994@gmail.com>
2023-09-08 11:27:07 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char star = 'a';
+	char end = 'z';
+
+	for(char cont = star, cont <= end, cont++)
+	{
+		putchar(cont);
+	}
+
+	putchar(\n);
+
+	return (0);
+
+}
