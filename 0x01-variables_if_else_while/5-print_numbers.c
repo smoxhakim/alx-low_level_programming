a1a1b33970fe31ff4a116a50392a884e3f3386e4
smoxhakim <joulal1994@gmail.com>
2023-09-08 13:09:13 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
+
 /*
  * main - entry point
  *
  * return always 0 
  */
+
 int main(void)
 {
 	int num[] = {0,1,2,3,4,5,6,7,8,9};
 	int x;
 	for (x = num; x <= 9; x++)
 	{
-		puts(x);
+		printf("%d", num[x]);
 	}
 
 	putchar('\n');
 
 	return (0);
 }
-
