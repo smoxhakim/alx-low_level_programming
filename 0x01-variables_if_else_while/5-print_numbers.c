586e134d11aa9dd51c743c76172a26668d5d057d
smoxhakim <joulal1994@gmail.com>
2023-09-08 13:15:27 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-
-/*
- * main - entry point
+/**
+ * main - Entry point
  *
- * return always 0
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	int num[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 	int x;
+
 	for (x = 0; x <= 9; x++)
 	{
 		printf("%d", num[x]);
