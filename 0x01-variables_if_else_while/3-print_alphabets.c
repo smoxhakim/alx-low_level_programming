86287e167311a3f10dbcea98cd5284e33148b7a2
smoxhakim <joulal1994@gmail.com>
2023-09-08 12:15:32 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char star = 'a';
+	char end = 'z';
+	char cont;
+	char starA = 'A'
+	char endZ = 'Z';
+	char cont1;
+	for (cont = star; cont <= end; cont++)
+	{
+		putchar(cont);
+	}
+	for (cont1 = starA; cont1 <= endZ; cont1++)
+	{
+		putchar(cont1);
+	}
+
+        putchar('\n');
+
+        return (0);
+
+}
