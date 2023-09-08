5a0f7b86a2ef9e19d5d3a8c900a9ede35ce5e051
smoxhakim <joulal1994@gmail.com>
2023-09-08 12:36:33 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char star = 'a';
+	char end = 'z';
+	char cont;
+
+	for (cont = star; cont <= end; cont++)
+	{
+		if(cont == 'e')
+		{
+			cot++;
+		}
+		else if(cont == 'q')
+		{
+			cont++;
+		}
+
+		putchar(cont);
+	}
+	for (cont1 = starA; cont1 <= endZ; cont1++)
+	{
+		putchar(cont1);
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
+
