c5ad3ce25cbb798dc8f652c284044ac7a9e1a1a9
smoxhakim <joulal1994@gmail.com>
2023-09-08 17:40:51 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char num[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		putchar(num[x]);
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
