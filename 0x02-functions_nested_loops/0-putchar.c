f25624c235f96b9c9e880f0d4b36659ef7718ede
smoxhakim <joulal1994@gmail.com>
2023-09-12 14:50:06 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/*
+ *main - start 
+ *
+ *return - 0
+ *
+ * */
+
+int main(void)
+{
+	char name[] = "_putchar";
+	int x;
+
+	for (x = 0; x < 8; x++)
+	{
+		_putchar(nane[x]);
+	}
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char ch);
+
+
+
+
+#endif
