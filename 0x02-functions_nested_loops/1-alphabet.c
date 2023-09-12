1609db33cb5c0e37d0a509f7c64fa1a6644b5553
smoxhakim <joulal1994@gmail.com>
2023-09-12 15:31:00 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet(void)
+ * the alphabet, in lowercase, followed by a new line.
+ * alphabet: a - z
+ */
+
+void print_alphabet(void)
+{
+	int alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		_putchar(alph);
+	}
+	_putchar('\n');
+}
