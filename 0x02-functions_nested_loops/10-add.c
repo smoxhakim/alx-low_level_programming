9553b9308d3afb5710a03224f4881763707af9f9
smoxhakim <joulal1994@gmail.com>
2023-09-13 14:01:06 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - this add tow nubers
+ * @a: the first input
+ * @b: the second input
+ * Return: the result of a & b
+ */
+
+int add(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+
+}
