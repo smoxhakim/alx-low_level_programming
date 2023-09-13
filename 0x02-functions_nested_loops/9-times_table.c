b88851c67e01de92af921d1def1475c9625ee555
smoxhakim <joulal1994@gmail.com>
2023-09-13 08:21:24 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * times_table - print the 9 time table
+ * starting with 0
+ *
+ */
+
+void times_table(void);
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = a; j <= 9; j++)
+		{
+			int result = i * j;
+
+			if (result < 10)
+			{
+				_putchar(result + '0');
+			}
+			else
+			{
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
+			}
+			_putchar('t');
+		}
+		_putchar('n');
+	}
+}
