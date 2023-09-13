3135dbc5bf842c02d3cc4d18923705ed14450446
smoxhakim <joulal1994@gmail.com>
2023-09-12 17:31:02 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character
+ * @c: charachter to be checked
+ * Return: 1 if lowercase or  if 0 is not lower
+ *
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
