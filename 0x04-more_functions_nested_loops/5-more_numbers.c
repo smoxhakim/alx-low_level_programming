804f8235c2ecf8c1f38256dedeac0c3c5a9a02c0
smoxhakim <joulal1994@gmail.com>
2023-09-14 14:06:59 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -16,8 +16,8 @@ void more_numbers(void)
 		{
 			if (a > 9)
 			{
-				_putchar((a / 10) + '0');
-				_putchar((a % 10) + '0');
+				_putchar(a / 10 + '0');
+				_putchar(a % 10 + '0');
 			}
 			else
 			{
