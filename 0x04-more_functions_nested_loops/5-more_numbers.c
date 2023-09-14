8d62389fd4be3a3fb1f6c3da2a259c4ea87ff9ea
smoxhakim <joulal1994@gmail.com>
2023-09-14 13:34:40 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - print the numbers from 0 to 9
+ *
+ */
+
+void print_numbers(void)
+{
+	int a;
+	int cont,
+
+	for (cont = 0; cont < 10; cont++)
+	{
+		for (a = 0; a <= 14; a++)
+		{
+			_putchar(a + '0');
+		}
+	_putchar('\n');
+	}
+}
