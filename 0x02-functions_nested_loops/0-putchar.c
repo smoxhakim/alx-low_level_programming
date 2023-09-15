f12254b4e2c9d095935efa3a12d22446a99414a7
smoxhakim <joulal1994@gmail.com>
2023-09-15 19:53:39 +0100

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char namep[] = "_putchar";
+	int x;
+
+	for (x = 0; x < 8; x++)
+	{
+		_putchar(name[x]);
+	}
+
+	_putchar('\n');
+	return (0);
+}
