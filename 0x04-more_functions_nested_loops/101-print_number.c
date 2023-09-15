0ec5fda0d1ccc318717e22ed1ac931b8c406ada6
smoxhakim <joulal1994@gmail.com>
2023-09-14 19:13:02 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,13 +1,15 @@
 #include "main.h"
+
 /**
  * print_number - prints an integer
  * @n: the input number
  */
+
 void print_number(int n)
 {
 	unsigned int number = n;
 
-	if (number < 0)
+	if (n < 0)
 	{
 		_putchar(45);
 
