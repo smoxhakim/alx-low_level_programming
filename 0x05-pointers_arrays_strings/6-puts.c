62dba33c5e6ee95945fffc86a83f80950ca7bc42
smoxhakim <joulal1994@gmail.com>
2023-09-18 18:57:37 +0100

    6-puts.c

---
+++ 0x05-pointers_arrays_strings/6-puts.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts2 - print other character
+ * @str: the input
+ */
+
+void puts2(char *str)
+{
+	int cont = 0;
+	int a;
+	int b;
+
+	while (str[cont] != '\0')
+	{
+		cont++
+	}
+
+	for (a = 0; a < cont - 1; a++)
+	{
+		b = a * 2;
+		_putchar(str[b]);
+	}
+	_putchar('\n');
+}
