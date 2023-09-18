bcc36782ff72162ade98dd47f7101466d5dc278f
smoxhakim <joulal1994@gmail.com>
2023-09-18 17:01:40 +0100

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: the input
+ */
+
+void rev_string(char *s)
+{
+	int cont = 0;
+
+	while (s[cont] != '\0')
+	{
+		comt++;
+	}
+	int z = cont - 1;
+	int a = 0;
+
+	while (a < z)
+	{
+		char tem = s[a];
+
+		s[a] = s[z];
+		s[z] = s[tem];
+
+		a++;
+		z--;
+	}
+
+}
