cae0ae8f1de63e96fd5140655f1a740d12539690
smoxhakim <joulal1994@gmail.com>
2023-09-18 19:07:19 +0100

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,18 +8,11 @@
 void puts2(char *str)
 {
 	int cont = 0;
-	int a;
-	int b;
 
 	while (str[cont] != '\0')
 	{
-		cont++;
-	}
-
-	for (a = 0; a < cont - 1; a++)
-	{
-		b = a * 2;
-		_putchar(str[b]);
+		_putchar(str[cont]);
+		cont += 2;
 	}
 	_putchar('\n');
 }
