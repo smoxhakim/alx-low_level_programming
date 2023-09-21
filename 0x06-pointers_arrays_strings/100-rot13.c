d53b39d62b09d245a2a04f73158e40d13cf67d10
smoxhakim <joulal1994@gmail.com>
2023-09-21 07:52:26 +0100

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string
+ * @s: input
+ * Return: s
+ * owned by: SmoxHakim
+ */
+
+char *rot13(char *s)
+{
+	int y = 0, x;
+	char alph[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	while (s[y] != '\0')
+	{
+		for (x = 0; x < 52; x++)
+		{
+			if (s[y] == alph[x])
+			{
+				s[y] = rot13[x];
+				break;
+			}
+		}
+		c++;
+	}
+
+	return (s);
+}
