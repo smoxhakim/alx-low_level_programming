8eb960e0fa87fbabc15dc8eb86de70178e13e938
smoxhakim <joulal1994@gmail.com>
2023-09-21 07:44:23 +0100

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,31 +2,35 @@
 
 /**
  * cap_string - capitalizes all words of a string
- * @str: input
- * Return: str
+ * @s: input
+ * Return: s
  * owned by: SmoxHakim
  */
 
-char *cap_string(char *str)
+char *cap_string(char *s)
 {
 	int c = 0, x;
 	int sep[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
 	if (s[c] >= 97 && s[c] <= 122)
+	{
 		s[c] = s[c] - 32;
+	}
 	c++;
-	while s[c] != '\0')
+	while (s[c] != '\0')
 	{
 		for (x = 0; x < 13; x++)
 		{
 			if (s[c] == sep[x])
 			{
 				if (s[c + 1] >= 97 && s[c + 1] <= 122)
+				{
 					s[c + 1] = s[c + 1] - 32;
+				}
 				break;
 			}
 		}
-		count++;
+		c++;
 	}
-	return (str);
+	return (s);
 }
