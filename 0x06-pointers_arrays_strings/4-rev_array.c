ec3a2ff5000d0238bc7c706c275d04e14e6d8052
smoxhakim <joulal1994@gmail.com>
2023-09-21 07:23:34 +0100

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * reverse_array - rverse two strings
+ * @a: first input
+ * @n: input number
+ * owned by: SmoxHakim
+ * Return: dest
+*/
+
+void reverse_array(int *a, int n)
+{
+	int x, y, tmp;
+
+	for (x = 0; x < n - 1; x++)
+	{
+		for (y = x + 1; y > 0; y--)
+		{
+			tmp = a[y];
+			a[y] = a[y - 1];
+			a[y - 1] = tmp;
+		}
+}
