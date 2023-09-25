fbe92f4f3309baea8a904d3b2f0e255d18ca84ad
smoxhakim <joulal1994@gmail.com>
2023-09-26 00:06:48 +0100

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the langh
+ * @s: the string
+ * @accept: the count
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int conunt = 0;
+
+	while (*s != '\0' && accept != NULL)
+	{
+		count++;
+		s++;
+	}
+}
