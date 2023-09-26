d143b766e2a4e04f2b34e72b8e296bc0829bccba
smoxhakim <joulal1994@gmail.com>
2023-09-26 01:19:04 +0100

    8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - prints the sum
+ * @a: input
+ * @size: size of the matrix
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int x;
+	int sum1 = 0, sum2 = 0;
+
+	for (x = 0; x < (size * size); x++)
+	{
+		if (x % (size + 1) == 0)
+		{
+			sum1 += *(a + x);
+		}
+		else if (x % (size - 1) == 0 && x != 0 && x < size * size - 1)
+		{
+			sum2 += *(a + x);
+		}
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
