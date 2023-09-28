84faddf82ed9bc6ab7e3165759c580fe5d46699e
smoxhakim <joulal1994@gmail.com>
2023-09-29 00:23:53 +0100

    100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * is_palindrome - check if palindrome
+ * @s: input
+ * Return: 1 or 0
+ * owned by: SmoxHakim
+ */
+
+int is_palindrome(char *s)
+{
+	if (s != "")
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
