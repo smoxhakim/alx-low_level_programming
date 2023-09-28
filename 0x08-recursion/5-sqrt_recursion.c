35d4369ee8b8dab1bb00c18c9aa1e11566f49fb4
smoxhakim <joulal1994@gmail.com>
2023-09-28 22:41:25 +0100

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - print the square
+ * @n: the number
+ * @ges: the gueess number
+ * Return: the square number of n or -1
+ * owned by: SmoxHakim
+ */
+
+int _sqrt_recursion(int n, int ges)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (ges * ges == n)
+	{
+		return (ges);
+	}
+	else if (ges * ges > n)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (_sqrt_recursion(n, ges + 1));
+	}
+}
+
+int _sqrt_recursion(int n)
+{
+	return (_sqrt_recursion(int n, 0));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,6 +8,8 @@ int factorial(int n);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+
 
 
 
