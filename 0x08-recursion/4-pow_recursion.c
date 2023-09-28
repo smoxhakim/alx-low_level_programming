af9e4f63ba6e5bf8355019afd177e461f4e848e7
smoxhakim <joulal1994@gmail.com>
2023-09-28 21:13:34 +0100

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include "nain.h"
+
+/**
+ * _pow_recursion - print the power of
+ * @x: the base number
+ * @y: the power number
+ * Return: the result
+ * owned by: SmoxHakim
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y > 0)
+	{
+		return (y * _pow_recursion(x * y - 1));
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (-1);
+	}
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,6 +7,7 @@ int _putchar(char ch);
 int factorial(int n);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
+int _pow_recursion(int x, int y);
 
 
 
