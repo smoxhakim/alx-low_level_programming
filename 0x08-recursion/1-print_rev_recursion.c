1031a6ef4749d5d00217954fae61780dfa4d7d6b
smoxhakim <joulal1994@gmail.com>
2023-09-28 18:18:16 +0100

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,13 +8,11 @@
 
 void _print_rev_recursion(char *s)
 {
-	int x = '\0';
+	int x = 0;
 
-	if (s[x] == '\0')
+	if (s[x] > '\0')
 	{
-		_putchar('\n');
-		x--;
+		_print_rev_recursion(s + 1);
 		_putchar(s[x]);
-		_print_rev_recursion(s - 1);
 	}
 }
