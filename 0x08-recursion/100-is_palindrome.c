8c7c147a38f9647d1415dbe51b9ac3ec9f0df3a8
smoxhakim <joulal1994@gmail.com>
2023-09-29 01:24:54 +0100

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -43,5 +43,6 @@ int _is_palindrome(char *s, int star, int end)
 
 int is_palindrome(char *s)
 {
-	return (_is_palindrome(s, 0, length - 1));
+	int len = length(s);
+	return (_is_palindrome(s, 0, len - 1));
 }
