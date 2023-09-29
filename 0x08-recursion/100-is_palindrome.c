4a91edf42b2861a2f35f669f3178291c22552880
smoxhakim <joulal1994@gmail.com>
2023-09-29 01:15:19 +0100

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,20 +1,47 @@
 #include "main.h"
 
 /**
+ * length - counting
+ * @s: the input string
+ * Return: 1 or 0
+ * owned by: SmoxHakim
+ */
+
+int length(int *s)
+{
+	if (*s != '\0')
+		return (1);
+	else
+		return (1 + length(s + 1));
+}
+
+/**
+ * _is_palindrome - check if palindrome
+ * @s: input
+ * @star: the start
+ * @end: the end
+ * Return: 1 or 0
+ * owned by: SmoxHakim
+ */
+
+int _is_palindrome(*s, int star, int end)
+{
+	if (star >= end)
+		return (1);
+	else if (s[star] != s[end])
+		return (0);
+	else
+		return (_is_palindrome(s, star + 1, end - 1));
+}
+
+/**
  * is_palindrome - check if palindrome
  * @s: input
- * Return: 1 or 0
+ * Return: result
  * owned by: SmoxHakim
  */
 
 int is_palindrome(char *s)
 {
-	if (*s == ' ')
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+	return (_is_palindrome(s, 0, length - 1));
 }
