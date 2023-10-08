5e7b047113b8d168cd4509cd7cd4b493ff168881
smoxhakim <joulal1994@gmail.com>
2023-10-08 17:44:23 +0100

    1-string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,44 +12,34 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len1;
-	unsigned int len2;
+	unsigned int len1 = 0;
+	unsigned int len2 = 0;
 	char *reslt;
 	unsigned int x, y;
 
 	if (!s1)
-	{
 		s1 = "";
-	}
 	if (!s2)
-	{
 		s2 = "";
-	}
-
-	len1 = mylenght(s1);
-	len2 = mylenght(s2);
-
+	
+	while (s1[len1])
+		len1++;
+	while (s2[len2])
+		len2++;
 	if (n >= len2)
-	{
 		n = len2;
-	}
-
+	
 	reslt = malloc(len1 + n + 1);
 
 	if (!reslt)
-	{
 		return (NULL);
-	}
-
+	
 	for (x = 0; x < len1; x++)
-	{
 		reslt[x] = s1[x];
-	}
-
+	
 	for (y = 0; y < n; y++)
-	{
 		reslt[x + y] = s2[y];
-	}
+
 	reslt[x + y] = '\0';
 	return (reslt);
 }
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,7 +2,6 @@
 #define MAIN_H
 
 void *malloc_checked(unsigned int b);
-unsigned int mylenght(char *s);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 
 
