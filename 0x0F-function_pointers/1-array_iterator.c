45a721fb3d3155dd9975ede6d9ee986ebde440c9
smoxhakim <joulal1994@gmail.com>
2023-10-11 22:17:15 +0100

    1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "funcyion_pointer.h"
+
+/**
+ * array_iterator - execute the given paratmeter
+ * @array: input array
+ * @size: size of the array
+ * @action: ptr to funcyion
+ * owned by: SmoxHakim
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t x;
+
+	if (array != NULL && action != NULL)
+	{
+		for (x = 0; x < size; x++)
+			action(array[x]);
+	}
+}
