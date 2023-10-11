1c4d6c6218e72ffc9a7c86458158c7c006ee2a9a
smoxhakim <joulal1994@gmail.com>
2023-10-11 03:05:10 +0100

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,28 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees the memory
+ * @d: pointer
+ * owned by: SmoxHakim
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+
+	{
+		if (d->name)
+		{
+		free(d->name);
+		}
+
+		if (d->owner)
+		{
+		free(d->owner);
+		}
+
+	free(d);
+	}
+}
+
