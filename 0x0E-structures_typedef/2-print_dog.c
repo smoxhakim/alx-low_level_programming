6d43852f791353fe363c5e3402dcc51e6b801f6c
smoxhakim <joulal1994@gmail.com>
2023-10-11 02:36:58 +0100

    2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - prints struct
+ * @d: the struct
+ * owned by: SmoxHakim
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+
+	printf("Name: %s\n", d->name ? d->name : "(nil)");
+	printf("Age: %.1f\n", d->age);
+	printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+
+	}
+}
