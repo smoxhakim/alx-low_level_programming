f07eb767967210a12bdf2f94278581e11a950cfa
AbdeljalilOuafi <joulal1994@gmail.com>
2023-10-19 23:16:46 +0100

    3-add_node_end.c

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * add_node_end - adds a new node
+ * @head: the head
+ * @str: the string
+ * Return: the address
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *nov, *swp;
+	size_t x;
+
+	novw = malloc(sizeof(list_t));
+	if (nov == NULL)
+		return (NULL);
+
+	nov->str = strdup(str);
+
+	for (x = 0; str[x]; x++)
+	{
+	}
+	nov->len = nchar;
+	nov->next = NULL;
+	swp = *head;
+
+	if (swp == NULL)
+	{
+		*head = nov;
+	}
+	else
+	{
+		while (swp->next != NULL)
+			swp = swp->next;
+		swp->next = nov;
+	}
+
+	return (*head);
+}
