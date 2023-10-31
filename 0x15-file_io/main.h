3570b1e1a20cc306a4506f18677aa0d4c7ef4989
AbdeljalilOuafi <joulal1994@gmail.com>
2023-10-31 20:57:12 +0100

    100-elf_header.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,12 +3,11 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <elf.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <elf.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
