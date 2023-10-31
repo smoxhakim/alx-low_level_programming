8412a9280ba6634cf88cc18021abba579ddc159d
AbdeljalilOuafi <joulal1994@gmail.com>
2023-10-31 21:03:03 +0100

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -57,19 +57,19 @@ void print_class(unsigned char *e_ident)
 
 void check_elf(unsigned char *e_ident)
 {
-        int i;
+	int i;
 
-        for (i = 0; i < 4; i++)
-        {
-                if (e_ident[i] != 127 &&
-                        e_ident[i] != 'E' &&
-                        e_ident[i] != 'L' &&
-                        e_ident[i] != 'F')
-                {
-                        dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
-                        exit(98);
-                }
-        }
+	for (i = 0; i < 4; i++)
+	{
+		if (e_ident[i] != 127 &&
+			e_ident[i] != 'E' &&
+			e_ident[i] != 'L' &&
+			e_ident[i] != 'F')
+		{
+			dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
+			exit(98);
+		}
+	}
 }
 
 /**
